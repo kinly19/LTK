b053fabb5c66dd72a29915498231943c45641522
unknown <kinchoily@hotmail.com>
2022-11-16 17:19:44 +0000

    Create 'Home' page component

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import Landing from '../../components/Landing/Landing';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <main className='home'>
+      <Landing />
+      {/* Info sections */}
+    </main>
+  );
+};
+
+export default Home;

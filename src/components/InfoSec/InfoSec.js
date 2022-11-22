4b43e37960f0e70c8b013ca5075eb84acb197830
unknown <kinchoily@hotmail.com>
2022-11-22 18:18:08 +0000

    InfoSec.js - Add 'Heading' component and inline style for background images

--- src/components/InfoSec/InfoSec.js
+++ src/components/InfoSec/InfoSec.js
@@ -1,9 +1,14 @@
+import Heading from '../Heading/Heading';
 import './InfoSec.scss';
 
 const InfoSec = (props) => {
+  const backgroundImg = props.backgroundImage && {
+    backgroundImage: `linear-gradient(0deg, rgba(39,39,39,0.975) 100%, rgba(39,39,39,0.975) 100%),url(${props.backgroundImage})`,
+  }; 
+
   return (
-    <section className="infoSec">
-      {props.heading}
+    <section className="infoSec" style={backgroundImg}>
+     <Heading>{props.heading}</Heading>
       <div className="infoSec__content">
         {props.children}
       </div>

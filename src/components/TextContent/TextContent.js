87eb7da09405dc3c615f89a9267f6c5595ffa2ca
unknown <kinchoily@hotmail.com>
2022-11-24 17:03:14 +0000

    TextContent.js - Add dynamic class styles to 'textContent' container

--- src/components/TextContent/TextContent.js
+++ src/components/TextContent/TextContent.js
@@ -1,8 +1,10 @@
 import './TextContent.scss';
 
 const TextContent = (props) => {
+  const containerStyle = { width: props.width, fontSize: props.fontSize}
+
   return (
-    <div className='textContent'>
+    <div className='textContent' style={containerStyle}>
       {props.children}
     </div>
   );

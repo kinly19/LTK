3fd093264d5a08fdec6c8cc89bc8431bbf6ea01f
unknown <kinchoily@hotmail.com>
2022-12-01 14:10:07 +0000

    Create 'Button' component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import './Button.scss';
+
+const Button = (props) => {
+  const navigate = useNavigate();
+  return (
+    <button
+      className={`button ${props.class}`}
+      onClick={() => navigate(props.redirectTo)}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;

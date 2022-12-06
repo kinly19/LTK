13ebbb3b8eb28af5bd3536cbd5c96781abe3da8a
unknown <kinchoily@hotmail.com>
2022-12-06 20:02:53 +0000

    ContentContainer.js - Add conditional class

--- src/components/Layout/Content/ContentContainer.js
+++ src/components/Layout/Content/ContentContainer.js
@@ -1,7 +1,11 @@
 import './ContentContainer.scss';
 
 const ContentContainer = (props) => {
-  return <div className="contentContainer">{props.children}</div>;
+  return (
+    <div className={!props.class ? "contentContainer" : `contentContainer ${props.class}`}>
+      {props.children}
+    </div>
+  );
 };
 
 export default ContentContainer;

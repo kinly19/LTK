b323b536f21c9ac308bcd7bd47134d66af25ba86
unknown <kinchoily@hotmail.com>
2022-12-13 17:14:09 +0000

    Create 'About' component page

---
+++ src/pages/About/About.js
@@ -0,0 +1,43 @@
+import PageLayout from '../../components/Layout/PageLayout/PageLayout';
+import ContentContainer from '../../components/Layout/Content/ContentContainer';
+import TextContainer from '../../components/Layout/Text/TextContainer';
+import Button from '../../components/Button/Button';
+import img1 from '../../assets/img/img1.jpg';
+import './About.scss';
+
+const About = () => {
+  return (
+    <PageLayout backgroundImage={img1} heading={"About Me"}>
+      <ContentContainer>
+        <img className="about__img" src={img1} alt="img1" />
+        <TextContainer>
+          <p>
+            Trained in wt style Taekwondo since 2009 and teaching From 2017,
+            with A life experience in both sport and traditional Aspects of
+            Martial Arts. During my training I have competed in a few, small UK
+            Competitions in sparring and Poomsae in the past.
+          </p>
+
+          <p>
+            I am a firm believer that it is never too late to start learning a
+            new profound skill, no matter how big or small or what others have
+            to say. To me a martial art is a lifelong journey. not only
+            developing myself pysically but mentally too, inside and outside of
+            training.
+          </p>
+
+          <p>
+            No matter what age, background or physical fitness level, everyone
+            is treated equally in a safe environment with respect. Classes are
+            tailored to meet individual goals and abilities, whether private or
+            group sessions (children and adults). Visit the workshop programmes
+            to find out more.
+          </p>
+        </TextContainer>
+        <Button redirectTo={"/classes"}>Classes</Button>
+      </ContentContainer>
+    </PageLayout>
+  );
+};
+
+export default About;

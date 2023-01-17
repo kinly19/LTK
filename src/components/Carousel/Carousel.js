30d91759e4902e65717eb5e1541a1bfdfb6f5da5
unknown <kinchoily@hotmail.com>
2023-01-17 19:17:55 +0000

    Carousel.js - Add handlers for 'ImgSlider' component

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,16 +1,29 @@
 import { useState, useEffect } from 'react';
 import CarouselButtons from './CarouselButtons/CarouselButtons';
+import CarouselItem from './CarouselItem/CarouselItem';
+import ImgSlider from '../ImgSlider/ImgSlider';
+import Img from '../Img/Img';
 import './Carousel.scss';
 
 const Carousel = (props) => {
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const [length, setLength] = useState(props.children.length);
+  const [selectedSlideIndex, setSelectedSlideIndex] = useState(null);
+  const [length, setLength] = useState(props.imgData.length);
   const [viewportWidth, setViewPortWidth] = useState(window.innerWidth);
+  const [showSlider, setShowSlider] = useState(false);
+  const imageArray = props.imgData;
 
   // Slides to show per view
   let slideAmount = 3;
   if (viewportWidth <= 768) slideAmount = 2;
   if (viewportWidth <= 400) slideAmount = 1;
+
+  // Carousel items
+  const carouselItems = imageArray.map((imgItem, index) => (
+    <CarouselItem onClick={() => toggleImageSliderHandler(index)} key={index}>
+      <Img imgSrc={imgItem} />
+    </CarouselItem>
+  ));
   
   const prevSlideHandler = () => {
     if (currentSlideIndex > 0)
@@ -26,10 +39,15 @@ const Carousel = (props) => {
     setViewPortWidth(window.innerWidth);
   };
 
+  const toggleImageSliderHandler = (index) => {
+    setShowSlider(!showSlider);
+    setSelectedSlideIndex(index);
+  }
+
   useEffect(() => {
-    setLength(props.children.length);
+    setLength(props.imgData.length);
     window.addEventListener("resize", resizeHandler);
-  }, [props.children, viewportWidth]);
+  }, [props.imgData.length, viewportWidth]);
 
   return (
     <div className="carousel">
@@ -38,17 +56,22 @@ const Carousel = (props) => {
           <div
             className={`carousel__content show-${slideAmount}`}
             style={{
-              transform: `translateX(-${currentSlideIndex * (100 / slideAmount)}%)`
+              transform: `translateX(-${currentSlideIndex * (100 / slideAmount)}%)`,
             }}
           >
-            {props.children}
+            {carouselItems}
           </div>
         </div>
       </div>
-      <CarouselButtons
-        onClickLeft={prevSlideHandler}
-        onClickRight={nextSlideHandler}
-      />
+      <CarouselButtons onClickLeft={prevSlideHandler} onClickRight={nextSlideHandler} />
+      
+      {showSlider && (
+        <ImgSlider
+          imageData={imageArray}
+          currentSlide={selectedSlideIndex}
+          onToggleSlider={toggleImageSliderHandler}
+        />
+      )}
     </div>
   );
 };

9577237b82ab5762f3d46ea5f99eaf8f34280a78
unknown <kinchoily@hotmail.com>
2023-01-17 19:08:17 +0000

    CarouselItem.js - Add 'onClick' handler

--- src/components/Carousel/CarouselItem/CarouselItem.js
+++ src/components/Carousel/CarouselItem/CarouselItem.js
@@ -2,7 +2,7 @@ import './CarouselItem.scss';
 
 const CarouselItem = (props) => {
   return (
-    <div className='carouselItem'>
+    <div className='carouselItem' onClick={props.onClick}>
       <div className='carouselItem__content'>
         {props.children}
       </div>

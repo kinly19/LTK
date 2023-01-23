ae69b551aacf08063ed6b948c04fcd6ff5884e2d
unknown <kinchoily@hotmail.com>
2023-01-23 15:40:04 +0000

    ContentContainer.js - Add additional props for container width

--- src/components/Layout/Content/ContentContainer.js
+++ src/components/Layout/Content/ContentContainer.js
@@ -1,8 +1,12 @@
 import './ContentContainer.scss';
 
 const ContentContainer = (props) => {
+  const containerStyle = {width: props.containerWidth};
   return (
-    <div className={!props.class ? "contentContainer" : `contentContainer ${props.class}`}>
+    <div
+      className={!props.class ? "contentContainer" : `contentContainer ${props.class}`}
+      style={containerStyle}
+    >
       {props.children}
     </div>
   );

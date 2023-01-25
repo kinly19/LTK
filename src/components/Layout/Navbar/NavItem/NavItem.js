a454a0699c6c8f94a713e75f5fa6baab9b5ed8c2
unknown <kinchoily@hotmail.com>
2023-01-25 16:09:41 +0000

    NavItem.js - Fix typo

--- src/components/Layout/Navbar/NavItem/NavItem.js
+++ src/components/Layout/Navbar/NavItem/NavItem.js
@@ -2,13 +2,13 @@ import { NavLink } from 'react-router-dom';
 import './NavItem.scss';
 
 const NavItem = (props) => {
-  const activeClasss = (navData) =>
+  const activeClass = (navData) =>
     navData.isActive ? "nav__item nav__item--active" : "nav__item";
     
   return (
     <li className="nav__item">
       <NavLink 
-        className={activeClasss} 
+        className={activeClass} 
         to={props.link} 
         onClick={props.handler}
       >

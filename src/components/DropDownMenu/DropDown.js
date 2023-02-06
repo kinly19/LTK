af898fdd19aea4743b0c6ff55c580bea6efb39f3
unknown <kinchoily@hotmail.com>
2023-02-06 20:56:21 +0000

    DropDown.js - Update logic for dropdown menu

--- src/components/DropDownMenu/DropDown.js
+++ src/components/DropDownMenu/DropDown.js
@@ -4,17 +4,32 @@ import './DropDown.scss';
 const DropDown = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const dropDownHandler = () => {
+  const toggleDropdownHandler = () => {
     setIsOpen(!isOpen);
+  };
+
+  const closeDropdownHandler = (e) => {
+    const currentTarget = e.currentTarget;
+    
+    requestAnimationFrame(() => {
+      // closure on currentTarget
+      // Handle blur if element is not directly inside of the parent (li)
+      if (!currentTarget.contains(document.activeElement)) {
+        // Allows child element click event to run before handler removes element from dom
+        setTimeout(() => {
+          setIsOpen(false);
+        }, 150);
+      }
+    });
   };
 
   return (
     <li
-      className={`${props.className}`}
-      onMouseEnter={dropDownHandler}
-      onMouseLeave={dropDownHandler}
+      className={`${props.class}`}
+      onClick={toggleDropdownHandler}
+      onBlur={closeDropdownHandler}
     >
-      More
+      <a href="javascript:void(0)">{props.title}</a>
       {isOpen && <ul className="dropDown__items">{props.children}</ul>}
     </li>
   );

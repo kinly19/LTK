3f1730041585424e5b47fadcbf7708c1fe22f878
unknown <kinchoily@hotmail.com>
2023-02-07 17:45:30 +0000

    DropDown.js - Update component and fix event bubbling issues for dropdown menu links

--- src/components/DropDownMenu/DropDown.js
+++ src/components/DropDownMenu/DropDown.js
@@ -1,24 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import './DropDown.scss';
 
 const DropDown = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const currentUrl = useLocation();
+  const toggleDropdownHandler = () => setIsOpen(!isOpen);
 
-  const toggleDropdownHandler = () => {
-    setIsOpen(!isOpen);
+  const propagationHandler = (e) => {
+    // Stop events from bubbling up to onBlur event
+    e.stopPropagation();
   };
+
+  useEffect(() => {
+    // Close dropdown menu on url change (for desktop devices)
+    setIsOpen(false);
+  }, [currentUrl]);
 
   const closeDropdownHandler = (e) => {
     const currentTarget = e.currentTarget;
-    
+
     requestAnimationFrame(() => {
       // closure on currentTarget
       // Handle blur if element is not directly inside of the parent (li)
       if (!currentTarget.contains(document.activeElement)) {
-        // Allows child element click event to run before handler removes element from dom
-        setTimeout(() => {
-          setIsOpen(false);
-        }, 150);
+        setIsOpen(false);
       }
     });
   };
@@ -26,11 +32,13 @@ const DropDown = (props) => {
   return (
     <li
       className={`${props.class}`}
-      onClick={toggleDropdownHandler}
+      onMouseDown={toggleDropdownHandler}
       onBlur={closeDropdownHandler}
     >
-      <a href="javascript:void(0)">{props.title}</a>
-      {isOpen && <ul className="dropDown__items">{props.children}</ul>}
+      <button className='dropDown__btn'>{props.title}</button>
+      {isOpen && <ul className="dropDown__items" onMouseDown={propagationHandler}>
+        {props.children}
+      </ul>}
     </li>
   );
 };

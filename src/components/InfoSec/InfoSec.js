856be44e16667f91e4b48ef59d58f9b3388ceb4a
unknown <kinchoily@hotmail.com>
2023-02-09 19:03:53 +0000

    InfoSec.js - Update component to allow use of 'ref'

--- src/components/InfoSec/InfoSec.js
+++ src/components/InfoSec/InfoSec.js
@@ -1,15 +1,16 @@
+import { forwardRef } from 'react';
 import './InfoSec.scss';
 
-const InfoSec = (props) => {
+const InfoSec = forwardRef((props, ref) => {
   const backgroundImg = props.backgroundImage && {
     backgroundImage: `linear-gradient(0deg, rgba(39,39,39,0.975) 100%, rgba(39,39,39,0.975) 100%),url(${props.backgroundImage})`,
   }; 
 
   return (
-    <section className="infoSec" style={backgroundImg}>
+    <section className="infoSec" ref={ref} style={backgroundImg}>
       {props.children}
     </section>
   );
-};
+});
 
 export default InfoSec;

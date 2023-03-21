525a968e3ae2dc754deec93accb5ac8c2258fffb
unknown <kinchoily@hotmail.com>
2023-03-21 16:08:54 +0000

    Form.js - Update component to handle user feedback on submit and conditional rendering of 'FormConfirmation' component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,10 @@
 import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Img from '../Img/Img';
 import FormInput from './FormInput/FormInput';
 import FormSelect from './FormSelect.js/FormSelect';
+import FormConfirmation from './FormConfirmation/FormConfirmation';
+import FormButton from '../Form/FormButton/FormButton';
 import img6 from '../../assets/img/img6.jpg';
 import './Form.scss';
 
@@ -26,8 +29,14 @@ const Form = () => {
 
   const [formData, setFormData] = useState(initFormData);
   const [error, setError] = useState({});
-  const [formError, setFormError] = useState(false)
+  const [formError, setFormError] = useState(false);
+  const [status, setStatus] = useState({
+    isLoading: false,
+    submit: false
+  })
+ 
   const formRef = useRef();
+  const navigate = useNavigate()
 
   // Regular expression for validation
   const ukNumberRegex = /^(?:(?:00)?44|0)7(?:[45789]\d{2}|624)\d{6}$/;
@@ -90,28 +99,58 @@ const Form = () => {
     setFormError(false);
   };
 
+  const scrollToElement = (element) => {
+    const elementTop = element?.current.getBoundingClientRect().top;
+    const pageYOffset = window.scrollY - 60;
+
+    window.scrollTo({
+      left: 0,
+      top: elementTop + pageYOffset,
+    });
+  };
+
+  const confirmationHandler = () => {
+    setStatus({
+      ...status,
+      isLoading: true,
+    });
+
+    setTimeout(() => {
+      setStatus({
+        ...status,
+        isLoading: false,
+      });
+      clearFormInputsHandler();
+      navigate("/");
+    }, 500);
+  };
+
   const formSubmitHandler = (e) => {
     e.preventDefault();
     // Validate form inputs
-    const inputErrors = validateFormHandler()
-
+    const inputErrors = validateFormHandler();
     // Check for input errors
     if (Object.keys(inputErrors).length > 0) {
       setError(inputErrors);
       setFormError(true);
-      formRef.current.scrollIntoView();
+      scrollToElement(formRef);
     } else {
-      // Handle confirmation
-      console.log("Form submitted");
+      // Handle form submit/loading
+      setStatus({
+        ...status,
+        isLoading: true,
+      });
+      
       setTimeout(() => {
-        console.log(
-          `Thank you ${formData.firstName} for booking with us, You should recieve an email from us confirming your booking. We look forwards to seeing you`
-        );
-        console.log(formData)
-        clearFormInputsHandler();
-      }, 1000);
-    }   
-  }
+        scrollToElement(formRef);
+        setStatus({
+          ...status,
+          submit: true,
+          isLoading: false,
+        });
+      }, 1200);
+    }
+  };
 
   return (
     <div className="form" onSubmit={formSubmitHandler} ref={formRef}>
@@ -120,171 +159,182 @@ const Form = () => {
       </div>
       {/* Form */}
       <form className="form__content">
-        {formError && (
-          <div className="form__error">
-            <p> * Please fill in all required fields before submitting the form. * </p>
-          </div>
+        {!status.submit && (
+          <>
+            {!formError && <h2>Please fill in the form below</h2>}
+            {formError && (
+              <div className="form__error">
+                <p>* Please fill in all required fields before submitting the form. *</p>
+              </div>
+            )}
+            {/* Form input fields */}
+            <div className="input-container">
+              <label>Student Name</label>
+              <div className="input-name-wrapper">
+                <FormInput
+                  type={"text"}
+                  placeholder={"First Name"}
+                  id={"firstName"}
+                  value={formData.firstName}
+                  onChange={(e) => inputHandler(e)}
+                  pattern={nameRegex.test(formData.firstName)}
+                  required
+                  errorMsg={error.firstName}
+                />
+
+                <FormInput
+                  type={"text"}
+                  placeholder={"Last Name"}
+                  id={"lastName"}
+                  value={formData.lastName}
+                  onChange={(e) => inputHandler(e)}
+                  pattern={nameRegex.test(formData.lastName)}
+                  required
+                  errorMsg={error.lastName}
+                />
+              </div>
+            </div>
+
+            <div className="input-container">
+              <label>Contact</label>
+              <FormInput
+                type={"text"}
+                placeholder="Number"
+                id={"contactNumber"}
+                value={formData.contactNumber}
+                onChange={(e) => inputHandler(e)}
+                pattern={ukNumberRegex.test(formData.contactNumber)}
+                required
+                errorMsg={error.contactNumber}
+              />
+            </div>
+
+            <div className="input-container">
+              <label>Email</label>
+              <FormInput
+                type={"email"}
+                placeholder="Email"
+                id={"email"}
+                value={formData.email}
+                onChange={(e) => inputHandler(e)}
+                pattern={formData.email.includes("@")}
+                required
+                errorMsg={error.email}
+              />
+            </div>
+
+            <div className="input-container">
+              <label>Address</label>
+              <FormInput
+                type={"text"}
+                placeholder={"Street"}
+                id={"street"}
+                name={"address"}
+                value={formData.address.street}
+                onChange={(e) => inputHandler(e)}
+                pattern={formData.address.street.length > 3}
+                required
+                errorMsg={error.street}
+              />
+
+              <FormInput
+                type={"text"}
+                placeholder={"City"}
+                id={"city"}
+                name={"address"}
+                value={formData.address.city}
+                onChange={(e) => inputHandler(e)}
+                pattern={formData.address.city.length > 3}
+                required
+                errorMsg={error.city}
+              />
+
+              <FormInput
+                type={"text"}
+                placeholder={"Postal Code"}
+                id={"postalCode"}
+                name={"address"}
+                value={formData.address.postalCode}
+                onChange={(e) => inputHandler(e)}
+                pattern={ukPostCodeRegex.test(formData.address.postalCode)}
+                required
+                errorMsg={error.postalCode}
+              />
+            </div>
+
+            <div className="input-container input-container--small">
+              <label>Date Of Birth</label>
+              <FormInput
+                type={"date"}
+                id={"dob"}
+                name={"dob"}
+                value={formData.dob}
+                onChange={(e) => inputHandler(e)}
+                pattern={formData.dob !== ""}
+                required
+                errorMsg={"Enter date of birth"}
+              />
+            </div>
+
+            {/* Form Select fields */}
+            <div className="input-container input-container--small">
+              <label>Gender</label>
+              <FormSelect
+                name={"gender"}
+                id={"gender"}
+                value={formData.gender}
+                onChange={(e) => inputHandler(e)}
+                pattern={formData.gender !== ""}
+                errorMsg={"Select Gender"}
+              >
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+              </FormSelect>
+            </div>
+
+            <div className="input-container input-container--small">
+              <label>Workshop</label>
+              <FormSelect
+                name={"workshop"}
+                id={"workshop"}
+                value={formData.workshop}
+                onChange={(e) => inputHandler(e)}
+                pattern={formData.workshop !== ""}
+                errorMsg={"Select a Workshop"}
+              >
+                <option value="">Select Workshop</option>
+                <option value="TKD Adults">TKD Adults - 7pm to 8pm (Wed & Fri)</option>
+                <option value="TKD Kids">TKD Kids - 10am to 11am (Saturday)</option>
+                <option value="TKD Private">TKD Private</option>
+              </FormSelect>
+            </div>
+
+            <div className="input-container input-container--small">
+              <label>Select date</label>
+              <FormInput
+                type={"date"}
+                id={"workshopDate"}
+                name={"workshopDate"}
+                value={formData.workshopDate}
+                onChange={(e) => inputHandler(e)}
+                pattern={formData.workshopDate !== ""}
+                required
+                errorMsg={"Please choose a date"}
+              />
+            </div>
+            <FormButton title={"Confirm Booking"} status={status.isLoading} />
+          </>
         )}
-        {!formError && <h2>Please fill in the form below</h2>}
-
-        {/* Form input fields */}
-        <div className="input-container">
-          <label>Student Name</label>
-          <div className="input-name-wrapper">
-            <FormInput
-              type={"text"}
-              placeholder={"First Name"}
-              id={"firstName"}
-              value={formData.firstName}
-              onChange={(e) => inputHandler(e)}
-              pattern={nameRegex.test(formData.firstName)}
-              required
-              errorMsg={error.firstName}
-            />
-
-            <FormInput
-              type={"text"}
-              placeholder={"Last Name"}
-              id={"lastName"}
-              value={formData.lastName}
-              onChange={(e) => inputHandler(e)}
-              pattern={nameRegex.test(formData.lastName)}
-              required
-              errorMsg={error.lastName}
-            />
-          </div>
-        </div>
-
-        <div className="input-container">
-          <label>Contact</label>
-          <FormInput
-            type={"text"}
-            placeholder="Number"
-            id={"contactNumber"}
-            value={formData.contactNumber}
-            onChange={(e) => inputHandler(e)}
-            pattern={ukNumberRegex.test(formData.contactNumber)}
-            required
-            errorMsg={error.contactNumber}
+        {/* Confirmation component */}
+        {status.submit && (
+          <FormConfirmation
+            formDetails={formData}
+            status={status.isLoading}
+            onClick={confirmationHandler}
           />
-        </div>
-
-        <div className="input-container">
-          <label>Email</label>
-          <FormInput
-            type={"email"}
-            placeholder="Email"
-            id={"email"}
-            value={formData.email}
-            onChange={(e) => inputHandler(e)}
-            pattern={formData.email.includes('@')}
-            required
-            errorMsg={error.email}
-          />
-        </div>
-
-        <div className="input-container">
-          <label>Address</label>
-          <FormInput
-            type={"text"}
-            placeholder={"Street"}
-            id={"street"}
-            name={"address"}
-            value={formData.address.street}
-            onChange={(e) => inputHandler(e)}
-            pattern={formData.address.street.length > 3}
-            required
-            errorMsg={error.street}
-          />
-
-          <FormInput
-            type={"text"}
-            placeholder={"City"}
-            id={"city"}
-            name={"address"}
-            value={formData.address.city}
-            onChange={(e) => inputHandler(e)}
-            pattern={formData.address.city.length > 3}
-            required
-            errorMsg={error.city}
-          />
-
-          <FormInput
-            type={"text"}
-            placeholder={"Postal Code"}
-            id={"postalCode"}
-            name={"address"}
-            value={formData.address.postalCode}
-            onChange={(e) => inputHandler(e)}
-            pattern={ukPostCodeRegex.test(formData.address.postalCode)}
-            required
-            errorMsg={error.postalCode}
-          />
-        </div>
-
-        <div className="input-container input-container--small">
-          <label>Date Of Birth</label>
-          <FormInput
-            type={"date"}
-            id={"dob"}
-            name={"dob"}
-            value={formData.dob}
-            onChange={(e) => inputHandler(e)}
-            pattern={formData.dob !== ""}
-            required
-            errorMsg={"Enter date of birth"}
-          />
-        </div>
-
-        {/* Form Select fields */}
-        <div className="input-container input-container--small">
-          <label>Gender</label>
-          <FormSelect
-            name={"gender"}
-            id={"gender"}
-            value={formData.gender}
-            onChange={(e) => inputHandler(e)}
-            pattern={formData.gender !== ""}
-            errorMsg={"Select Gender"}
-          >
-            <option value="">Select Gender</option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Other">Other</option>
-          </FormSelect>
-        </div>
-
-        <div className="input-container input-container--small">
-          <label>Workshop</label>
-          <FormSelect
-            name={"workshop"}
-            id={"workshop"}
-            value={formData.workshop}
-            onChange={(e) => inputHandler(e)}
-            pattern={formData.workshop !== ""}
-            errorMsg={"Select a Workshop"}
-          >
-            <option value="">Select Workshop</option>
-            <option value="TKD Adults">TKD Adults - 7pm to 8pm (Wed & Fri)</option>
-            <option value="TKD Kids">TKD Kids - 10am to 11am (Saturday)</option>
-            <option value="TKD Private">TKD Private</option>
-          </FormSelect>
-        </div>
-
-        <div className="input-container input-container--small">
-          <label>Select date</label>
-          <FormInput
-            type={"date"}
-            id={"workshopDate"}
-            name={"workshopDate"}
-            value={formData.workshopDate}
-            onChange={(e) => inputHandler(e)}
-            pattern={formData.workshopDate !== ""}
-            required
-            errorMsg={"Please choose a date"}
-          />
-        </div>
-        <button className="mt-3">Confirm Booking</button>
+        )}
       </form>
     </div>
   );
